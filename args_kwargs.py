617c072c1ac33fe3e781d25251032b848f65dca0
7minus2 <mss73@njit.edu>
2020-04-05 20:52:50 -0400

    add *args **kwargs excercise

---
+++ args_kwargs.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+
+def sum(num1, num2):
+    '''
+    Info: This adds 2 numbers \n
+    Example: sum(1,2) returns 3
+    '''
+    return num1 + num2
+
+
+def my_sum2(*args, **kwargs):
+    '''
+    Info: *args accepts multiple values as tuple (1,2,3,4,5) \n
+    **kwargs needs num1 and num2 for sum() function
+    '''
+    total = 0
+    print(f'args {args} kwargs: {kwargs}', end='\n')
+    for item in args:
+        total += item
+    return total + sum(**kwargs)
+
+
+print(my_sum2(1, 2, 3, 4, 5, num1=5, num2=10))
