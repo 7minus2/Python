9b25cfd62983393c519b7fea76a3ddb8bffef8fe
7minus2 <mss73@njit.edu>
2020-05-17 14:44:27 -0400

    more modules

--- Class Excercises/useful_modules.py
+++ Class Excercises/useful_modules.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
 from collections import Counter, defaultdict, OrderedDict
-
-
+import datetime
+import time
+from array import array
 # creates a dict of how many times number occurs in a iterable
 li = [1, 2, 3, 4, 5, 6, 7, 7]
 sentence = "Something jumped over the sheet"
@@ -19,6 +20,10 @@ d2 = OrderedDict()
 d2['a'] = 1
 d2['b'] = 2
 
+# lists in python are dynamic, increase as much as possible
+# arrays = less memory alternate to generators
+# https://docs.python.org/3/library/array.html
+arr = array('i', [1, 2, 3, 4])
 
 if __name__ == "__main__":
     print(Counter(li))
@@ -26,3 +31,7 @@ if __name__ == "__main__":
     print(dictionary['c'])
     # normal dictionary would be false, python3.7 + newer has ordered dict by default
     print(d2 == d)  # true
+    print(f'Today\'s date is : ', datetime.date.today())
+    print(time.asctime())
+    print(datetime.tzinfo())
+    print(arr)
