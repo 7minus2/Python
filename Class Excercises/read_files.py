423f2903a28e13c86a56d287135be3a86d87ddf1
7minus2 <mss73@njit.edu>
2020-05-17 17:00:00 -0400

    read file execercise

---
+++ Class Excercises/read_files.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+from time import time
+
+
+def my_timer_func(fn):
+    def wrapper(*args, **kwargs):
+        timer1 = time()
+        result = fn(*args, **kwargs)
+        timer2 = time()
+        return result, timer1, timer2
+    return wrapper
+
+
+@my_timer_func
+def reviewTestFile(file, mode):
+    my_file = open(file=file, mode=mode)
+    print(my_file.read())
+
+
+if __name__ == '__main__':
+    print('Running Solution')
+    func, timer1, timer2 = reviewTestFile(file='../files/test.txt', mode='r')
+    print(f'It took {timer2 - timer1} secs')
