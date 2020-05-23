8e95e896a3df033ae1b292629f86a8cc7040bbac
7minus2 <mss73@njit.edu>
2020-05-22 21:49:05 -0400

    more tests

--- Excercises/couter_excercise.py
+++ Excercises/couter_excercise.py
@@ -4,21 +4,26 @@
 # my_list = [1,2,3,4,5,6,7,8,9,10]
 
 
-def counter_checker1():
+def counter_checker1(num):
     # Solution 1
-    my_list = sum([x for x in range(1, 11, 1)])
-    print(f'Solution 1 Sum is: {my_list}', end='\n')  # Answer = 55
+    try:
+        my_list = sum([x for x in range(1, int(num), 1)])
+        return my_list
+    except ValueError as err:
+        return err
 
 
-def counter_checker2():
+def counter_checker2(num):
     # Solution 2
     total_sum = 0
-    for num in range(1, 11):
+    for num in range(1, num, 1):
         total_sum += num
-    print(f'Solution 2 Sum is: {total_sum}', end='\n')  # Answer = 55
+    return total_sum
 
 
 if __name__ == '__main__':
-    counter_checker1()
-    counter_checker2()
+    solution1 = counter_checker1('')
+    print(f'Solution 1 Sum is: {solution1}', end='\n')  # Answer = 55
+    solution2 = counter_checker2(11)
+    print(f'Solution 2 Sum is: {solution2}', end='\n')  # Answer = 55
     print("All done!")
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import unittest
 from Excercises.highest_even import highest_even
+from Excercises.couter_excercise import counter_checker1, counter_checker2
 
 
 class TestMain(unittest.TestCase):
@@ -9,5 +10,26 @@ class TestMain(unittest.TestCase):
         result = highest_even(my_list)
         self.assertEqual(result, 10)
 
+    def test_counter(self):
+        num = 11
+        result = counter_checker1(num)
+        self.assertEqual(result, 55)
 
-unittest.main()
+    def test_counter1(self):
+        num = ''
+        result = counter_checker1(num)
+        self.assertIsInstance(result, ValueError)
+
+    def test_counter1b(self):
+        num = 'straasdf'
+        result = counter_checker1(num)
+        self.assertIsInstance(result, ValueError)
+
+    def test_counter2(self):
+        num = 12
+        result = counter_checker2(num)
+        self.assertEqual(result, 66)
+
+
+if __name__ == '__main__':
+    unittest.main()
